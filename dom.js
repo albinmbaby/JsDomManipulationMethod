e4312775565a63028a8e81c0be45df0b23cf350c
albinmbaby <albinmbaby@gmail.com>
2024-08-03 07:14:50 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,9 @@
+function changelogo(){
+    let logo = document.getElementById("logo")
+    logo.src = "./images/googlelogo.png"
+}
+
+function reset(){
+    let logo = document.getElementById("logo")
+    logo.src = "./images/entrilogo.png"
+}
